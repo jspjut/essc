236e690e6c824f2875d6a9d2524da89f52678eb5
Josef Spjut <josef.spjut@gmail.com>
2024-06-08 22:56:25 -0400

    Move data to json for easy changes

--- script.js
+++ script.js
@@ -1,69 +1,91 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const devices = {
-        ipad_air_11: { width: 2360, height: 1640 },
-        ipad_pro_11: { width: 2420, height: 1668 },
-        lenovo_duet_10: { width: 1920, height: 1080 }
-    };
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const devices = data.devices;
+            const useCases = data.useCases;
 
-    const useCases = {
-        single_page: 1.55,
-        dual_page_portrait: 1 / 1.29,
-        dual_page_landscape: 1.29
-    };
+            const deviceInputs = ['device1', 'device2', 'device3'];
+            const useCaseInputs = ['use_case1', 'use_case2', 'use_case3'];
 
-    const deviceInputs = ['device1', 'device2', 'device3'];
-    const useCaseInputs = ['use_case1', 'use_case2', 'use_case3'];
+            // Populate dropdowns with options
+            deviceInputs.forEach((id, index) => {
+                const select = document.getElementById(id);
+                devices.forEach((device, deviceIndex) => {
+                    const option = document.createElement('option');
+                    option.value = device.id;
+                    option.textContent = device.name;
+                    if (index === deviceIndex) {
+                        option.selected = true;
+                    }
+                    select.appendChild(option);
+                });
+            });
 
-    function calculate() {
-        const device1 = devices[document.getElementById('device1').value];
-        const device2 = devices[document.getElementById('device2').value];
-        const device3 = devices[document.getElementById('device3').value];
-        const useCase1 = useCases[document.getElementById('use_case1').value];
-        const useCase2 = useCases[document.getElementById('use_case2').value];
-        const useCase3 = useCases[document.getElementById('use_case3').value];
+            useCaseInputs.forEach((id, index) => {
+                const select = document.getElementById(id);
+                useCases.forEach((useCase, useCaseIndex) => {
+                    const option = document.createElement('option');
+                    option.value = useCase.id;
+                    option.textContent = useCase.name;
+                    if (index === useCaseIndex) {
+                        option.selected = true;
+                    }
+                    select.appendChild(option);
+                });
+            });
 
-        updateResult('result1', device1, useCase1);
-        updateResult('result2', device2, useCase1);
-        updateResult('result3', device3, useCase1);
-        updateResult('result4', device1, useCase2);
-        updateResult('result5', device2, useCase2);
-        updateResult('result6', device3, useCase2);
-        updateResult('result7', device1, useCase3);
-        updateResult('result8', device2, useCase3);
-        updateResult('result9', device3, useCase3);
-    }
+            function calculate() {
+                const device1 = devices.find(device => device.id === document.getElementById('device1').value);
+                const device2 = devices.find(device => device.id === document.getElementById('device2').value);
+                const device3 = devices.find(device => device.id === document.getElementById('device3').value);
+                const useCase1 = useCases.find(useCase => useCase.id === document.getElementById('use_case1').value);
+                const useCase2 = useCases.find(useCase => useCase.id === document.getElementById('use_case2').value);
+                const useCase3 = useCases.find(useCase => useCase.id === document.getElementById('use_case3').value);
 
-    function updateResult(resultId, device, aspectRatio) {
-        const effectiveScreenSize = calculateEffectiveScreenSize(device, aspectRatio);
-        const fontSize = calculateFontSize(device, aspectRatio);
-        document.getElementById(resultId).innerHTML = `Effective Screen Size: ${effectiveScreenSize.toFixed(2)}"<br>10pt Font Size: ${fontSize.toFixed(2)}px`;
-    }
+                updateResult('result1', device1, useCase1);
+                updateResult('result2', device2, useCase1);
+                updateResult('result3', device3, useCase1);
+                updateResult('result4', device1, useCase2);
+                updateResult('result5', device2, useCase2);
+                updateResult('result6', device3, useCase2);
+                updateResult('result7', device1, useCase3);
+                updateResult('result8', device2, useCase3);
+                updateResult('result9', device3, useCase3);
+            }
 
-    function calculateEffectiveScreenSize(device, aspectRatio) {
-        let effectiveWidth, effectiveHeight;
-        if (aspectRatio === useCases.dual_page_landscape) {
-            effectiveWidth = device.width;
-            effectiveHeight = device.height / 2;
-        } else {
-            effectiveWidth = device.width / 2;
-            effectiveHeight = device.height;
-        }
-        return Math.sqrt(Math.pow(effectiveWidth, 2) + Math.pow(effectiveHeight, 2)) / 264;
-    }
+            function updateResult(resultId, device, useCase) {
+                const effectiveScreenSize = calculateEffectiveScreenSize(device, useCase.aspectRatio);
+                const fontSize = calculateFontSize(device, useCase.aspectRatio);
+                document.getElementById(resultId).innerHTML = `Effective Screen Size: ${effectiveScreenSize.toFixed(2)}"<br>10pt Font Size: ${fontSize.toFixed(2)}px`;
+            }
 
-    function calculateFontSize(device, aspectRatio) {
-        let effectiveHeight;
-        if (aspectRatio === useCases.dual_page_landscape) {
-            effectiveHeight = device.height / 2;
-        } else {
-            effectiveHeight = device.height;
-        }
-        return (10 / 72) * 96 * (effectiveHeight / device.height);
-    }
+            function calculateEffectiveScreenSize(device, aspectRatio) {
+                let effectiveWidth, effectiveHeight;
+                if (aspectRatio === 1.29) {
+                    effectiveWidth = device.width;
+                    effectiveHeight = device.height / 2;
+                } else {
+                    effectiveWidth = device.width / 2;
+                    effectiveHeight = device.height;
+                }
+                return Math.sqrt(Math.pow(effectiveWidth, 2) + Math.pow(effectiveHeight, 2)) / 264;
+            }
 
-    deviceInputs.concat(useCaseInputs).forEach(id => {
-        document.getElementById(id).addEventListener('change', calculate);
-    });
+            function calculateFontSize(device, aspectRatio) {
+                let effectiveHeight;
+                if (aspectRatio === 1.29) {
+                    effectiveHeight = device.height / 2;
+                } else {
+                    effectiveHeight = device.height;
+                }
+                return (10 / 72) * 96 * (effectiveHeight / device.height);
+            }
 
-    calculate();
+            deviceInputs.concat(useCaseInputs).forEach(id => {
+                document.getElementById(id).addEventListener('change', calculate);
+            });
+
+            calculate();
+        });
 });
